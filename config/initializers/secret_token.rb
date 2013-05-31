5cc1b0ded421f00e5df53fed8a2a48b499d88ac2
Dmitry Bragin <dbragin@aboutecho.com>
2013-06-01 00:16:19 +0400

    Initialized the empty rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Denquirer::Application.config.secret_token = 'c9279b83ddb292fa5123ce164e699cb23ac6aeac88ac9cb55630b163692b2bb925aa53ae6030faca7b69c3cefab66406f832839f2961d0741563d4cddb377f19'
